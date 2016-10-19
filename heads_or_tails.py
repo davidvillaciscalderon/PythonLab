ee39e46cb83bda5b289fdd482cbcb90b6574ae75
David Villacis C <villacis@vt.edu>
2016-10-19 18:13:12 -0400

    update

--- heads_or_tails.py
+++ heads_or_tails.py
@@ -5,10 +5,10 @@ guess=input("heads or tails")
 
 if random_number%2==0:
     coin="tails"
-else
+else:
     coin="heads"
 
 if guess==coin:
     print("You win")
-else
+else:
     print("You loose")
