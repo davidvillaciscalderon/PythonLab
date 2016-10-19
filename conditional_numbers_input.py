a795188948287b3e49821c0dffb256650bee2b66
David Villacis C <villacis@vt.edu>
2016-10-19 18:07:05 -0400

    update

--- conditional_numbers_input.py
+++ conditional_numbers_input.py
@@ -5,3 +5,8 @@ if number%2==0:
     print(str(number)+" is even")
 else:
     print(str(number)+" is odd")
+
+if number<17:
+    print(str(number)+" is smaller than 17")
+else:
+    print(str(number)+" is greater than 17")
